1b6495e5099741cbf40afc57fcdc343fcc384606
Elizabeth Hanly <e.hanly@comcast.net>
2022-06-17 13:43:37 -0700

    updating url in deploy

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.95.10.63',
+      host: '44.203.132.24',
       username: 'ubuntu',
       privateKey: '/Users/ehanl/Desktop/labsuser.pem'
     })
